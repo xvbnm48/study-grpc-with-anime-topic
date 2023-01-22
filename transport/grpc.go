3deccb2d366471f86cd75447123f304da58566b8
fariz <fariz.wisnu@bluebirdgroup.com>
2023-01-22 20:00:00 +0700

    transport, readme

---
+++ transport/grpc.go
@@ -0,0 +1,13 @@
+package transport
+
+import gt "github.com/go-kit/kit/transport/grpc"
+
+type Server interface {
+	Serve(addr string)
+	Stop()
+}
+
+type grpcServer struct {
+	add gt.Handler
+	get gt.Handler
+}
